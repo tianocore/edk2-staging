9a1a0216debf46c848577fb0a7a92588879f4018
Michael D Kinney <michael.d.kinney@intel.com>
2020-01-17 16:11:47 -0800

    UnitTestFrameworkPkg: Remove MockLibraryClassLibNull
    Signed-off-by: Michael D Kinney <michael.d.kinney@intel.com>

--- UnitTestFrameworkPkg/Library/MockLibraryClassLibNull/MockLibraryClassLibNull.c
+++
@@ -1,9 +0,0 @@
-/** @file -- MockLibraryClassLibNull.c
-Generic library that could be anything. It could be a boat!
-
-Copyright (c) Microsoft Corporation.
-SPDX-License-Identifier: BSD-2-Clause-Patent
-
-**/
-
-// I'm a comment!
