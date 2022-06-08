jyao1/edk2

Add DeviceSecurityPkg POC.
Signed-off-by: Jiewen Yao <jiewen.yao@intel.com>

DeviceSecurityPkg/Include/PrivateTest/Protocol/SpdmTest.h
@@ -1,0 +1,127 @@
+/** @file
+  SPDM Test Protocol definition
+
+  Copyright (c) 2020, Intel Corporation. All rights reserved.<BR>
+  SPDX-License-Identifier: BSD-2-Clause-Patent
+
+**/
+
+#ifndef __SPDM_TEST_PROTOCOL_H__
+#define __SPDM_TEST_PROTOCOL_H__
+
+#include <Base.h>
+#include <IndustryStandard/Spdm.h>
+#include <Protocol/Spdm.h>
+
+typedef struct _SPDM_TEST_PROTOCOL SPDM_TEST_PROTOCOL;
+
+/**
+  Set a SPDM Session Data.
+
+  @param  This                         Indicates a pointer to the calling context.
+  @param  DataType                     Type of the session data.
+  @param  Data                         A pointer to the session data.
+  @param  DataSize                     Size of the session data.
+
+  @retval RETURN_SUCCESS                  The SPDM session data is set successfully.
+  @retval RETURN_INVALID_PARAMETER        The Data is NULL or the DataType is zero.
+  @retval RETURN_UNSUPPORTED              The DataType is unsupported.
+  @retval RETURN_ACCESS_DENIED            The DataType cannot be set.
+  @retval RETURN_NOT_READY                Current session is not started.
+**/
+typedef
+RETURN_STATUS
+(EFIAPI *SPDM_TEST_SET_DATA) (
+  IN     SPDM_TEST_PROTOCOL  *This,
+  IN     SPDM_DATA_TYPE      DataType,
+  IN     SPDM_DATA_PARAMETER *Parameter,
+  IN     VOID                      *Data,
+  IN     UINTN                     DataSize
+  );
+
+/**
+  Get a SPDM Session Data.
+
+  @param  This                         Indicates a pointer to the calling context.
+  @param  DataType                     Type of the session data.
+  @param  Data                         A pointer to the session data.
+  @param  DataSize                     Size of the session data. On input, it means the size of Data
+                                       buffer. On output, it means the size of copied Data buffer if
+                                       RETURN_SUCCESS, and means the size of desired Data buffer if
+                                       RETURN_BUFFER_TOO_SMALL.
+
+  @retval RETURN_SUCCESS                  The SPDM session data is set successfully.
+  @retval RETURN_INVALID_PARAMETER        The DataSize is NULL or the Data is NULL and *DataSize is not zero.
+  @retval RETURN_UNSUPPORTED              The DataType is unsupported.
+  @retval RETURN_NOT_FOUND                The DataType cannot be found.
+  @retval RETURN_NOT_READY                The DataType is not ready to return.
+  @retval RETURN_BUFFER_TOO_SMALL         The buffer is too small to hold the data.
+**/
+typedef
+RETURN_STATUS
+(EFIAPI *SPDM_TEST_GET_DATA) (
+  IN     SPDM_TEST_PROTOCOL  *This,
+  IN     SPDM_DATA_TYPE      DataType,
+  IN     SPDM_DATA_PARAMETER *Parameter,
+  IN OUT VOID                      *Data,
+  IN OUT UINTN                     *DataSize
+  );
+
+
+/**
+  Process a packet in the current SPDM session.
+
+  @param  This                         Indicates a pointer to the calling context.
+  @param  SessionId                    ID of the session.
+  @param  Request                      A pointer to the request data.
+  @param  RequestSize                  Size of the request data.
+  @param  Response                     A pointer to the response data.
+  @param  ResponseSize                 Size of the response data.
+                                       On output, it means the size of copied Data buffer if
+                                       RETURN_SUCCESS, and means the size of desired Data buffer if
+                                       RETURN_BUFFER_TOO_SMALL.
+
+  @retval RETURN_SUCCESS                  The SPDM request is set successfully.
+  @retval RETURN_INVALID_PARAMETER        The DataSize is NULL or the Data is NULL and *DataSize is not zero.
+  @retval RETURN_UNSUPPORTED              The DataType is unsupported.
+  @retval RETURN_NOT_FOUND                The DataType cannot be found.
+  @retval RETURN_NOT_READY                The DataType is not ready to return.
+  @retval RETURN_BUFFER_TOO_SMALL         The buffer is too small to hold the data.
+  @retval RETURN_TIMEOUT                  A timeout occurred while waiting for the SPDM request
+                                       to execute.
+**/
+typedef
+RETURN_STATUS
+(EFIAPI *SPDM_TEST_PROCESS_PACKET_CALLBACK) (
+  IN     UINT32                       SessionId,
+  IN     VOID                         *Request,
+  IN     UINTN                        RequestSize,
+     OUT VOID                         *Response,
+  IN OUT UINTN                        *ResponseSize
+  );
+
+
+/**
+  Register a callback function to process a packet in the current SPDM session.
+
+  @param  This                         Indicates a pointer to the calling context.
+  @param  Callback                     Process packet callback function.
+
+  @retval RETURN_SUCCESS                  The SPDM callback is registered successfully.
+**/
+typedef
+RETURN_STATUS
+(EFIAPI *SPDM_TEST_REGISTER_PROCESS_PACKET_CALLBACK) (
+  IN     SPDM_TEST_PROTOCOL                     *This,
+  IN     SPDM_TEST_PROCESS_PACKET_CALLBACK      Callback
+  );
+
+struct _SPDM_TEST_PROTOCOL {
+  SPDM_TEST_SET_DATA                          SetData;
+  SPDM_TEST_GET_DATA                          GetData;
+  SPDM_TEST_REGISTER_PROCESS_PACKET_CALLBACK  RegisterProcessPacketCallback;
+};
+
+extern GUID gSpdmTestProtocolGuid;
+
+#endif

DeviceSecurityPkg/Test/DeviceSecurityPolicyStub/DeviceSecurityPolicyStub.c
@@ -1,0 +1,203 @@
+/** @file
+  EDKII Device Security Policy Stub
+
+Copyright (c) 2020, Intel Corporation. All rights reserved.<BR>
+SPDX-License-Identifier: BSD-2-Clause-Patent
+
+**/
+
+#include <Uefi.h>
+#include <IndustryStandard/Spdm.h>
+#include <IndustryStandard/Pci.h>
+#include <Protocol/PciIo.h>
+#include <Protocol/DeviceSecurity.h>
+#include <Protocol/DeviceSecurityPolicy.h>
+#include <Library/DebugLib.h>
+#include <Library/BaseMemoryLib.h>
+#include <Library/UefiBootServicesTableLib.h>
+#include <Library/MemoryAllocationLib.h>
+
+EDKII_DEVICE_SECURITY_POLICY           mDeviceSecurityPolicyNone = {
+  EDKII_DEVICE_SECURITY_POLICY_REVISION,
+  0,
+  0,
+};
+
+EDKII_DEVICE_SECURITY_POLICY           mDeviceSecurityPolicyFull = {
+  EDKII_DEVICE_SECURITY_POLICY_REVISION,
+  EDKII_DEVICE_MEASUREMENT_REQUIRED,
+  EDKII_DEVICE_AUTHENTICATION_REQUIRED
+};
+
+/**
+  This function returns the device security policy associated with the device.
+
+  The device security driver may call this interface to get the platform policy
+  for the specific device and determine if the measurement or authentication
+  is required.
+
+  @param[in]  This                   The protocol instance pointer.
+  @param[in]  DeviceId               The Identifier for the device.
+  @param[out] DeviceSecurityPolicy   The Device Security Policy associated with the device.
+
+  @retval EFI_SUCCESS                The device security policy is returned
+  @retval EFI_UNSUPPORTED            The function is unsupported for the specific Device.
+**/
+EFI_STATUS
+EFIAPI
+GetDevicePolicy (
+  IN  EDKII_DEVICE_SECURITY_POLICY_PROTOCOL  *This,
+  IN  EDKII_DEVICE_IDENTIFIER                *DeviceId,
+  OUT EDKII_DEVICE_SECURITY_POLICY           *DeviceSecurityPolicy
+  )
+{
+  EFI_STATUS                  Status;
+  EFI_PCI_IO_PROTOCOL         *PciIo;
+  UINT16                      PciVendorId;
+  UINT16                      PciDeviceId;
+
+  CopyMem (DeviceSecurityPolicy, &mDeviceSecurityPolicyNone, sizeof(EDKII_DEVICE_SECURITY_POLICY));
+
+  DEBUG ((DEBUG_INFO, "GetDevicePolicy - 0x%g\n", &DeviceId->DeviceType));
+
+  if (!CompareGuid (&DeviceId->DeviceType, &gEdkiiDeviceIdentifierTypePciGuid)) {
+    return EFI_SUCCESS;
+  }
+
+  Status = gBS->HandleProtocol (
+                  DeviceId->DeviceHandle,
+                  &gEdkiiDeviceIdentifierTypePciGuid,
+                  (VOID **)&PciIo
+                  );
+  if (EFI_ERROR(Status)) {
+    DEBUG ((DEBUG_ERROR, "Locate - DeviceIdentifierTypePci - %r\n", Status));
+    return EFI_SUCCESS;
+  }
+
+  Status = PciIo->Pci.Read (PciIo, EfiPciIoWidthUint16, PCI_VENDOR_ID_OFFSET, 1, &PciVendorId);
+  ASSERT_EFI_ERROR(Status);
+  Status = PciIo->Pci.Read (PciIo, EfiPciIoWidthUint16, PCI_DEVICE_ID_OFFSET, 1, &PciDeviceId);
+  ASSERT_EFI_ERROR(Status);
+  DEBUG ((DEBUG_INFO, "PCI Info - %04x:%04x\n", PciVendorId, PciDeviceId));
+
+  CopyMem (DeviceSecurityPolicy, &mDeviceSecurityPolicyFull, sizeof(EDKII_DEVICE_SECURITY_POLICY));
+
+  return EFI_SUCCESS;
+}
+
+/**
+  This function sets the device state based upon the authentication result.
+
+  The device security driver may call this interface to give the platform
+  a notify based upon the measurement or authentication result.
+  If the authentication or measurement fails, the platform may choose:
+  1) Do nothing.
+  2) Disable this device or slot temporarily and continue boot.
+  3) Reset the platform and retry again.
+  4) Disable this device or slot permanently.
+  5) Any other platform specific action.
+
+  @param[in]  This                   The protocol instance pointer.
+  @param[in]  DeviceId               The Identifier for the device.
+  @param[in]  DeviceSecurityState    The Device Security state associated with the device.
+
+  @retval EFI_SUCCESS                The device state is set
+  @retval EFI_UNSUPPORTED            The function is unsupported for the specific Device.
+**/
+EFI_STATUS
+EFIAPI
+NotifyDeviceState (
+  IN  EDKII_DEVICE_SECURITY_POLICY_PROTOCOL  *This,
+  IN  EDKII_DEVICE_IDENTIFIER                *DeviceId,
+  IN  EDKII_DEVICE_SECURITY_STATE            *DeviceSecurityState
+  )
+{
+  EFI_STATUS                  Status;
+  EFI_PCI_IO_PROTOCOL         *PciIo;
+  UINT16                      PciVendorId;
+  UINT16                      PciDeviceId;
+  UINTN                       Segment;
+  UINTN                       Bus;
+  UINTN                       Device;
+  UINTN                       Function;
+
+  DEBUG ((DEBUG_INFO, "NotifyDeviceState - 0x%g\n", &DeviceId->DeviceType));
+
+  if (!CompareGuid (&DeviceId->DeviceType, &gEdkiiDeviceIdentifierTypePciGuid)) {
+    return EFI_SUCCESS;
+  }
+
+  Status = gBS->HandleProtocol (
+                  DeviceId->DeviceHandle,
+                  &gEdkiiDeviceIdentifierTypePciGuid,
+                  (VOID **)&PciIo
+                  );
+  if (EFI_ERROR(Status)) {
+    DEBUG ((DEBUG_ERROR, "Locate - DeviceIdentifierTypePci - %r\n", Status));
+    return EFI_SUCCESS;
+  }
+
+  Status = PciIo->Pci.Read (PciIo, EfiPciIoWidthUint16, PCI_VENDOR_ID_OFFSET, 1, &PciVendorId);
+  ASSERT_EFI_ERROR(Status);
+  Status = PciIo->Pci.Read (PciIo, EfiPciIoWidthUint16, PCI_DEVICE_ID_OFFSET, 1, &PciDeviceId);
+  ASSERT_EFI_ERROR(Status);
+  DEBUG ((DEBUG_INFO, "PCI Info - %04x:%04x\n", PciVendorId, PciDeviceId));
+
+  Status = PciIo->GetLocation (
+                    PciIo,
+                    &Segment,
+                    &Bus,
+                    &Device,
+                    &Function
+                    );
+  if (!EFI_ERROR(Status)) {
+    DEBUG ((DEBUG_INFO, "PCI Loc - %04x:%02x:%02x:%02x\n",
+      Segment, Bus, Device, Function));
+  }
+
+  DEBUG ((DEBUG_INFO, "State - Measurement - 0x%08x, Authentication - 0x%08x\n",
+    DeviceSecurityState->MeasurementState,
+    DeviceSecurityState->AuthenticationState
+    ));
+
+  return EFI_SUCCESS;
+}
+
+EDKII_DEVICE_SECURITY_POLICY_PROTOCOL  mDeviceSecurityPolicy = {
+  EDKII_DEVICE_SECURITY_POLICY_PROTOCOL_REVISION,
+  GetDevicePolicy,
+  NotifyDeviceState,
+};
+
+/**
+  Entrypoint of the device security driver.
+
+  @param[in]  ImageHandle  ImageHandle of the loaded driver
+  @param[in]  SystemTable  Pointer to the System Table
+
+  @retval  EFI_SUCCESS           The Protocol is installed.
+  @retval  EFI_OUT_OF_RESOURCES  Not enough resources available to initialize driver.
+  @retval  EFI_DEVICE_ERROR      A device error occurred attempting to initialize the driver.
+
+**/
+EFI_STATUS
+EFIAPI
+MainEntryPoint (
+  IN EFI_HANDLE        ImageHandle,
+  IN EFI_SYSTEM_TABLE  *SystemTable
+  )
+{
+  EFI_HANDLE  Handle;
+  EFI_STATUS  Status;
+
+  Handle = NULL;
+  Status = gBS->InstallProtocolInterface (
+                  &Handle,
+                  &gEdkiiDeviceSecurityPolicyProtocolGuid,
+                  EFI_NATIVE_INTERFACE,
+                  &mDeviceSecurityPolicy
+                  );
+  ASSERT_EFI_ERROR(Status);
+
+  return Status;
+}

tianocore/edk2-staging

Signed-off-by: Qi Zhang <qi1.zhang@intel.com>

DeviceSecurityPkg/Include/PrivateTest/Protocol/SpdmTest.h
@@ -1,0 +1,125 @@
+/** @file
+  SPDM Test Protocol definition
+
+  Copyright (c) 2020, Intel Corporation. All rights reserved.<BR>
+  SPDX-License-Identifier: BSD-2-Clause-Patent
+
+**/
+
+#ifndef __SPDM_TEST_PROTOCOL_H__
+#define __SPDM_TEST_PROTOCOL_H__
+
+#include <Base.h>
+#include <industry_standard/spdm.h>
+#include <Protocol/Spdm.h>
+
+typedef struct _SPDM_TEST_PROTOCOL SPDM_TEST_PROTOCOL;
+
+/**
+  Set a SPDM Session Data.
+
+  @param  This                         Indicates a pointer to the calling context.
+  @param  DataType                     Type of the session data.
+  @param  Data                         A pointer to the session data.
+  @param  DataSize                     Size of the session data.
+
+  @retval RETURN_SUCCESS                  The SPDM session data is set successfully.
+  @retval RETURN_INVALID_PARAMETER        The Data is NULL or the DataType is zero.
+  @retval RETURN_UNSUPPORTED              The DataType is unsupported.
+  @retval RETURN_ACCESS_DENIED            The DataType cannot be set.
+  @retval RETURN_NOT_READY                Current session is not started.
+**/
+typedef
+RETURN_STATUS
+(EFIAPI *SPDM_TEST_SET_DATA) (
+  IN     SPDM_TEST_PROTOCOL        *This,
+  IN     SPDM_DATA_TYPE            DataType,
+  IN     SPDM_DATA_PARAMETER       *Parameter,
+  IN     VOID                      *Data,
+  IN     UINTN                     DataSize
+  );
+
+/**
+  Get a SPDM Session Data.
+
+  @param  This                         Indicates a pointer to the calling context.
+  @param  DataType                     Type of the session data.
+  @param  Data                         A pointer to the session data.
+  @param  DataSize                     Size of the session data. On input, it means the size of Data
+                                       buffer. On output, it means the size of copied Data buffer if
+                                       RETURN_SUCCESS, and means the size of desired Data buffer if
+                                       RETURN_BUFFER_TOO_SMALL.
+
+  @retval RETURN_SUCCESS                  The SPDM session data is set successfully.
+  @retval RETURN_INVALID_PARAMETER        The DataSize is NULL or the Data is NULL and *DataSize is not zero.
+  @retval RETURN_UNSUPPORTED              The DataType is unsupported.
+  @retval RETURN_NOT_FOUND                The DataType cannot be found.
+  @retval RETURN_NOT_READY                The DataType is not ready to return.
+  @retval RETURN_BUFFER_TOO_SMALL         The buffer is too small to hold the data.
+**/
+typedef
+RETURN_STATUS
+(EFIAPI *SPDM_TEST_GET_DATA) (
+  IN     SPDM_TEST_PROTOCOL        *This,
+  IN     SPDM_DATA_TYPE            DataType,
+  IN     SPDM_DATA_PARAMETER       *Parameter,
+  IN OUT VOID                      *Data,
+  IN OUT UINTN                     *DataSize
+  );
+
+
+/**
+  Process a packet in the current SPDM session.
+
+  @param  This                         Indicates a pointer to the calling context.
+  @param  Request                      A pointer to the request data.
+  @param  RequestSize                  Size of the request data.
+  @param  Response                     A pointer to the response data.
+  @param  ResponseSize                 Size of the response data.
+                                       On output, it means the size of copied Data buffer if
+                                       RETURN_SUCCESS, and means the size of desired Data buffer if
+                                       RETURN_BUFFER_TOO_SMALL.
+
+  @retval RETURN_SUCCESS                  The SPDM request is set successfully.
+  @retval RETURN_INVALID_PARAMETER        The DataSize is NULL or the Data is NULL and *DataSize is not zero.
+  @retval RETURN_UNSUPPORTED              The DataType is unsupported.
+  @retval RETURN_NOT_FOUND                The DataType cannot be found.
+  @retval RETURN_NOT_READY                The DataType is not ready to return.
+  @retval RETURN_BUFFER_TOO_SMALL         The buffer is too small to hold the data.
+  @retval RETURN_TIMEOUT                  A timeout occurred while waiting for the SPDM request
+                                       to execute.
+**/
+typedef
+RETURN_STATUS
+(EFIAPI *SPDM_TEST_PROCESS_PACKET_CALLBACK) (
+  IN     VOID                         *Request,
+  IN     UINTN                        RequestSize,
+     OUT VOID                         *Response,
+  IN OUT UINTN                        *ResponseSize
+  );
+
+
+/**
+  Register a callback function to process a packet in the current SPDM session.
+
+  @param  This                         Indicates a pointer to the calling context.
+  @param  Callback                     Process packet callback function.
+
+  @retval RETURN_SUCCESS                  The SPDM callback is registered successfully.
+**/
+typedef
+RETURN_STATUS
+(EFIAPI *SPDM_TEST_REGISTER_PROCESS_PACKET_CALLBACK) (
+  IN     SPDM_TEST_PROTOCOL                     *This,
+  IN     SPDM_TEST_PROCESS_PACKET_CALLBACK      Callback
+  );
+
+struct _SPDM_TEST_PROTOCOL {
+  SPDM_TEST_SET_DATA                          SetData;
+  SPDM_TEST_GET_DATA                          GetData;
+  SPDM_TEST_REGISTER_PROCESS_PACKET_CALLBACK  RegisterProcessPacketCallback;
+};
+
+extern GUID gSpdmTestProtocolGuid;
+
+#endif

DeviceSecurityPkg/Test/DeviceSecurityPolicyStub/DeviceSecurityPolicyStub.c
@@ -1,0 +1,203 @@
+/** @file
+  EDKII Device Security Policy Stub
+
+Copyright (c) 2020, Intel Corporation. All rights reserved.<BR>
+SPDX-License-Identifier: BSD-2-Clause-Patent
+
+**/
+
+#include <Uefi.h>
+#include <industry_standard/spdm.h>
+#include <IndustryStandard/Pci.h>
+#include <Protocol/PciIo.h>
+#include <Protocol/DeviceSecurity.h>
+#include <Protocol/DeviceSecurityPolicy.h>
+#include <Library/DebugLib.h>
+#include <Library/BaseMemoryLib.h>
+#include <Library/UefiBootServicesTableLib.h>
+#include <Library/MemoryAllocationLib.h>
+
+EDKII_DEVICE_SECURITY_POLICY           mDeviceSecurityPolicyNone = {
+  EDKII_DEVICE_SECURITY_POLICY_REVISION,
+  0,
+  0,
+};
+
+EDKII_DEVICE_SECURITY_POLICY           mDeviceSecurityPolicyFull = {
+  EDKII_DEVICE_SECURITY_POLICY_REVISION,
+  EDKII_DEVICE_MEASUREMENT_REQUIRED,
+  EDKII_DEVICE_AUTHENTICATION_REQUIRED
+};
+
+/**
+  This function returns the device security policy associated with the device.
+
+  The device security driver may call this interface to get the platform policy
+  for the specific device and determine if the measurement or authentication
+  is required.
+
+  @param[in]  This                   The protocol instance pointer.
+  @param[in]  DeviceId               The Identifier for the device.
+  @param[out] DeviceSecurityPolicy   The Device Security Policy associated with the device.
+
+  @retval EFI_SUCCESS                The device security policy is returned
+  @retval EFI_UNSUPPORTED            The function is unsupported for the specific Device.
+**/
+EFI_STATUS
+EFIAPI
+GetDevicePolicy (
+  IN  EDKII_DEVICE_SECURITY_POLICY_PROTOCOL  *This,
+  IN  EDKII_DEVICE_IDENTIFIER                *DeviceId,
+  OUT EDKII_DEVICE_SECURITY_POLICY           *DeviceSecurityPolicy
+  )
+{
+  EFI_STATUS                  Status;
+  EFI_PCI_IO_PROTOCOL         *PciIo;
+  UINT16                      PciVendorId;
+  UINT16                      PciDeviceId;
+
+  CopyMem (DeviceSecurityPolicy, &mDeviceSecurityPolicyNone, sizeof(EDKII_DEVICE_SECURITY_POLICY));
+
+  DEBUG ((DEBUG_INFO, "GetDevicePolicy - 0x%g\n", &DeviceId->DeviceType));
+
+  if (!CompareGuid (&DeviceId->DeviceType, &gEdkiiDeviceIdentifierTypePciGuid)) {
+    return EFI_SUCCESS;
+  }
+
+  Status = gBS->HandleProtocol (
+                  DeviceId->DeviceHandle,
+                  &gEdkiiDeviceIdentifierTypePciGuid,
+                  (VOID **)&PciIo
+                  );
+  if (EFI_ERROR(Status)) {
+    DEBUG ((DEBUG_ERROR, "Locate - DeviceIdentifierTypePci - %r\n", Status));
+    return EFI_SUCCESS;
+  }
+
+  Status = PciIo->Pci.Read (PciIo, EfiPciIoWidthUint16, PCI_VENDOR_ID_OFFSET, 1, &PciVendorId);
+  ASSERT_EFI_ERROR(Status);
+  Status = PciIo->Pci.Read (PciIo, EfiPciIoWidthUint16, PCI_DEVICE_ID_OFFSET, 1, &PciDeviceId);
+  ASSERT_EFI_ERROR(Status);
+  DEBUG ((DEBUG_INFO, "PCI Info - %04x:%04x\n", PciVendorId, PciDeviceId));
+
+  CopyMem (DeviceSecurityPolicy, &mDeviceSecurityPolicyFull, sizeof(EDKII_DEVICE_SECURITY_POLICY));
+
+  return EFI_SUCCESS;
+}
+
+/**
+  This function sets the device state based upon the authentication result.
+
+  The device security driver may call this interface to give the platform
+  a notify based upon the measurement or authentication result.
+  If the authentication or measurement fails, the platform may choose:
+  1) Do nothing.
+  2) Disable this device or slot temporarily and continue boot.
+  3) Reset the platform and retry again.
+  4) Disable this device or slot permanently.
+  5) Any other platform specific action.
+
+  @param[in]  This                   The protocol instance pointer.
+  @param[in]  DeviceId               The Identifier for the device.
+  @param[in]  DeviceSecurityState    The Device Security state associated with the device.
+
+  @retval EFI_SUCCESS                The device state is set
+  @retval EFI_UNSUPPORTED            The function is unsupported for the specific Device.
+**/
+EFI_STATUS
+EFIAPI
+NotifyDeviceState (
+  IN  EDKII_DEVICE_SECURITY_POLICY_PROTOCOL  *This,
+  IN  EDKII_DEVICE_IDENTIFIER                *DeviceId,
+  IN  EDKII_DEVICE_SECURITY_STATE            *DeviceSecurityState
+  )
+{
+  EFI_STATUS                  Status;
+  EFI_PCI_IO_PROTOCOL         *PciIo;
+  UINT16                      PciVendorId;
+  UINT16                      PciDeviceId;
+  UINTN                       Segment;
+  UINTN                       Bus;
+  UINTN                       Device;
+  UINTN                       Function;
+
+  DEBUG ((DEBUG_INFO, "NotifyDeviceState - 0x%g\n", &DeviceId->DeviceType));
+
+  if (!CompareGuid (&DeviceId->DeviceType, &gEdkiiDeviceIdentifierTypePciGuid)) {
+    return EFI_SUCCESS;
+  }
+
+  Status = gBS->HandleProtocol (
+                  DeviceId->DeviceHandle,
+                  &gEdkiiDeviceIdentifierTypePciGuid,
+                  (VOID **)&PciIo
+                  );
+  if (EFI_ERROR(Status)) {
+    DEBUG ((DEBUG_ERROR, "Locate - DeviceIdentifierTypePci - %r\n", Status));
+    return EFI_SUCCESS;
+  }
+
+  Status = PciIo->Pci.Read (PciIo, EfiPciIoWidthUint16, PCI_VENDOR_ID_OFFSET, 1, &PciVendorId);
+  ASSERT_EFI_ERROR(Status);
+  Status = PciIo->Pci.Read (PciIo, EfiPciIoWidthUint16, PCI_DEVICE_ID_OFFSET, 1, &PciDeviceId);
+  ASSERT_EFI_ERROR(Status);
+  DEBUG ((DEBUG_INFO, "PCI Info - %04x:%04x\n", PciVendorId, PciDeviceId));
+
+  Status = PciIo->GetLocation (
+                    PciIo,
+                    &Segment,
+                    &Bus,
+                    &Device,
+                    &Function
+                    );
+  if (!EFI_ERROR(Status)) {
+    DEBUG ((DEBUG_INFO, "PCI Loc - %04x:%02x:%02x:%02x\n",
+      Segment, Bus, Device, Function));
+  }
+
+  DEBUG ((DEBUG_INFO, "State - Measurement - 0x%08x, Authentication - 0x%08x\n",
+    DeviceSecurityState->MeasurementState,
+    DeviceSecurityState->AuthenticationState
+    ));
+
+  return EFI_SUCCESS;
+}
+
+EDKII_DEVICE_SECURITY_POLICY_PROTOCOL  mDeviceSecurityPolicy = {
+  EDKII_DEVICE_SECURITY_POLICY_PROTOCOL_REVISION,
+  GetDevicePolicy,
+  NotifyDeviceState,
+};
+
+/**
+  Entrypoint of the device security driver.
+
+  @param[in]  ImageHandle  ImageHandle of the loaded driver
+  @param[in]  SystemTable  Pointer to the System Table
+
+  @retval  EFI_SUCCESS           The Protocol is installed.
+  @retval  EFI_OUT_OF_RESOURCES  Not enough resources available to initialize driver.
+  @retval  EFI_DEVICE_ERROR      A device error occurred attempting to initialize the driver.
+
+**/
+EFI_STATUS
+EFIAPI
+MainEntryPoint (
+  IN EFI_HANDLE        ImageHandle,
+  IN EFI_SYSTEM_TABLE  *SystemTable
+  )
+{
+  EFI_HANDLE  Handle;
+  EFI_STATUS  Status;
+
+  Handle = NULL;
+  Status = gBS->InstallProtocolInterface (
+                  &Handle,
+                  &gEdkiiDeviceSecurityPolicyProtocolGuid,
+                  EFI_NATIVE_INTERFACE,
+                  &mDeviceSecurityPolicy
+                  );
+  ASSERT_EFI_ERROR(Status);
+
+  return Status;
+}