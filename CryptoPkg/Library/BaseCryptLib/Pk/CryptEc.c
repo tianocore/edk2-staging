61de49b1a51ea6e505955095df9c52bd00a1ecb3
Yi Li <yi1.li@intel.com>
2022-07-19 10:47:45 +0800

    CryptoPkg: New EcDh function demo
    Signed-off-by: Yi Li <yi1.li@intel.com>

--- CryptoPkg/Library/BaseCryptLib/Pk/CryptEc.c
+++ CryptoPkg/Library/BaseCryptLib/Pk/CryptEc.c
@@ -12,72 +12,104 @@ SPDX-License-Identifier: BSD-2-Clause-Patent
 #include "InternalCryptLib.h"
 #include <openssl/bn.h>
 #include <openssl/ec.h>
+#include <openssl/evp.h>
 #include <openssl/objects.h>
 
-/**
-  Allocates and Initializes one Elliptic Curve Context for subsequent use
-  with the NID.
-  
-  @param Nid cipher NID
-
-  @return  Pointer to the Elliptic Curve Context that has been initialized.
-           If the allocations fails, EcNewByNid() returns NULL.
-
-**/
-VOID *
-EFIAPI
-EcNewByNid (
-  IN UINTN  Nid
-  )
+STATIC
+INT32
+CryptoNidToOpensslNid (
+  IN UINTN CryptoNid
+)
 {
-  EC_KEY   *EcKey;
-  EC_GROUP *Group;
-  BOOLEAN  RetVal;
-  INT32    OpenSslNid;
+  INT32  Nid;
 
-  EcKey = EC_KEY_new();
-  if (EcKey == NULL) {
-    return NULL;
+  switch (CryptoNid) {
+    case CRYPTO_NID_SECP256R1:
+      Nid = NID_X9_62_prime256v1;
+      break;
+    case CRYPTO_NID_SECP384R1:
+      Nid = NID_secp384r1;
+      break;
+    case CRYPTO_NID_SECP521R1:
+      Nid = NID_secp521r1;
+      break;
+    default:
+      return -1;
   }
-  switch (Nid) {
-  case CRYPTO_NID_SECP256R1:
-    OpenSslNid = NID_X9_62_prime256v1;
-    break;
-  case CRYPTO_NID_SECP384R1:
-    OpenSslNid = NID_secp384r1;
-    break;
-  case CRYPTO_NID_SECP521R1:
-    OpenSslNid = NID_secp521r1;
-    break;
-  default:
-    return NULL;
-  }
-  
-  Group = EC_GROUP_new_by_curve_name(OpenSslNid);
-  if (Group == NULL) {
-    return NULL;
-  }
-  RetVal = (BOOLEAN) EC_KEY_set_group(EcKey, Group);
-  EC_GROUP_free(Group);
-  if (!RetVal) {
-    return NULL;
-  }
-  return (VOID *)EcKey;
+
+  return Nid;
 }
 
 /**
-  Release the specified EC context.
-  
-  @param[in]  EcContext  Pointer to the EC context to be released.
+  Initialize new opaque EcGroup object. This object represents an EC curve and
+  and is used for calculation within this group. This object should be freed
+  using EcGroupFree() function.
 
+  @param[in]  CryptoNid  Identifying number for the ECC group (IANA "Group
+                         Description" attribute registry for RFC 2409).
+
+  @retval EcGroup object  On success.
+  @retval NULL            On failure.
+**/
+VOID *
+EFIAPI
+EcGroupInit (
+  IN UINTN  CryptoNid
+  )
+{
+  INT32  Nid;
+
+  Nid = CryptoNidToOpensslNid (CryptoNid);
+
+  if (Nid < 0) {
+    return NULL;
+  }
+
+  return EC_GROUP_new_by_curve_name (Nid);
+}
+
+/**
+  Free previously allocated EC group object using EcGroupInit().
+
+
+  @param[in]  EcGroup   EC group object to free.
 **/
 VOID
 EFIAPI
-EcFree (
-  IN  VOID  *EcContext
+EcGroupFree (
+  IN VOID  *EcGroup
   )
 {
-  EC_KEY_free ((EC_KEY *) EcContext);
+  EC_GROUP_free (EcGroup);
+}
+
+/**
+  Free ECDH Key object previously created by EcDhGenKey() or .
+
+  @param[in] PKey  ECDH Key.
+**/
+VOID
+EFIAPI
+EcDhKeyFree (
+  IN VOID  *PKey
+  )
+{
+  EVP_PKEY_free (PKey);
+}
+
+STATIC
+UINTN
+EFIAPI
+EcGroupGetPrimeBytes (
+  IN VOID *EcGroup
+  )
+{
+  if (EcGroup == NULL) {
+    return 0;
+  } else {
+    // EC_GROUP_get_degree() will return the bits number of prime in EcGroup.
+    return (EC_GROUP_get_degree (EcGroup) + 7) / 8;
+  }
 }
 
 /**
@@ -98,80 +130,85 @@ EcFree (
 BOOLEAN
 EFIAPI
 EcSetPubKey (
-  IN OUT  VOID   *EcContext,
-  IN      UINT8  *PublicKey,
-  IN      UINTN  PublicKeySize
+  IN OUT  VOID     *PKey,
+  IN      EC_GROUP *EcGroup,
+  IN      UINT8    *PublicKey,
+  IN      UINTN    PublicKeySize,
+  IN      UINT32   *IncY
   )
 {
   EC_KEY         *EcKey;
-  CONST EC_GROUP *Group;
   BOOLEAN        RetVal;
   BIGNUM         *BnX;
   BIGNUM         *BnY;
-  EC_POINT       *Point;
-  INT32          OpenSslNid;
+  EC_POINT       *EcPoint;
   UINTN          HalfSize;
 
-  if (EcContext == NULL || PublicKey == NULL) {
+  if (PublicKey == NULL || EcGroup == NULL) {
     return FALSE;
   }
 
-  EcKey = (EC_KEY *)EcContext;
-  OpenSslNid = EC_GROUP_get_curve_name(EC_KEY_get0_group(EcKey));
-  switch (OpenSslNid) {
-  case NID_X9_62_prime256v1:
-    HalfSize = 32;
-    break;
-  case NID_secp384r1:
-    HalfSize = 48;
-    break;
-  case NID_secp521r1:
-    HalfSize = 66;
-    break;
-  default:
+  HalfSize = EcGroupGetPrimeBytes (EcGroup);
+  if ((IncY == NULL) && (PublicKeySize != HalfSize * 2)) {
     return FALSE;
   }
-  if (PublicKeySize != HalfSize * 2) {
+  //Compressed coordinates
+  if ((IncY != NULL) && (PublicKeySize != HalfSize)) {
     return FALSE;
   }
 
-  Group = EC_KEY_get0_group (EcKey);
-  Point = NULL;
+  EcKey   = NULL;
+  BnX     = NULL;
+  BnY     = NULL;
+  EcPoint = NULL;
+  RetVal  = FALSE;
 
   BnX = BN_bin2bn (PublicKey, (UINT32) HalfSize, NULL);
-  BnY = BN_bin2bn (PublicKey + HalfSize, (UINT32) HalfSize, NULL);
-  if (BnX == NULL || BnY == NULL) {
-    RetVal = FALSE;
-    goto Done;
-  }
-  Point = EC_POINT_new(Group);
-  if (Point == NULL) {
-    RetVal = FALSE;
+  EcPoint = EC_POINT_new(EcGroup);
+  if ((BnX == NULL) || (EcPoint == NULL)) {
     goto Done;
   }
 
-  RetVal = (BOOLEAN) EC_POINT_set_affine_coordinates(Group, Point, BnX, BnY, NULL);
-  if (!RetVal) {
+	if (IncY == NULL) {
+		BnY = BN_bin2bn(PublicKey + HalfSize, HalfSize, NULL);
+		if (BnY == NULL) {
+			goto Done;
+    }
+    if (EC_POINT_set_affine_coordinates (EcGroup, EcPoint, BnX, BnY, NULL) != 1) {
+			goto Done;
+		}
+	} else {
+    //Compressed coordinates
+    if (EC_POINT_set_compressed_coordinates(EcGroup, EcPoint, BnX, *IncY, NULL) != 1) {
+			goto Done;
+		}
+	}
+
+  //EC_KEY* function will be deprecated in openssl 3.0, need update here when OpensslLib updating.
+  EcKey = EC_KEY_new_by_curve_name (EC_GROUP_get_curve_name(EcGroup));
+  if ((EcKey == NULL) || (EC_KEY_set_public_key (EcKey, EcPoint) != 1)) {
     goto Done;
   }
 
-  RetVal = (BOOLEAN) EC_KEY_set_public_key (EcKey, Point);
-  if (!RetVal) {
-    goto Done;
+  if (PKey == NULL) {
+    PKey = EVP_PKEY_new ();
+    if ((PKey == NULL) || (EVP_PKEY_set1_EC_KEY (PKey, EcKey) != 1)) {
+      EVP_PKEY_free (PKey);
+      goto Done;
+    }
+  } else {
+    if (EVP_PKEY_set1_EC_KEY (PKey, EcKey) != 1) {
+      goto Done;
+    }
   }
 
   RetVal = TRUE;
 
 Done:
-  if (BnX != NULL) {
-    BN_free (BnX);
-  }
-  if (BnY != NULL) {
-    BN_free (BnY);
-  }
-  if (Point != NULL) {
-    EC_POINT_free(Point);
-  }
+  BN_free (BnX);
+  BN_free (BnY);
+  EC_POINT_free(EcPoint);
+  EC_KEY_free (EcKey);
   return RetVal;
 }
 
@@ -191,96 +228,80 @@ Done:
   @retval  FALSE  Invalid EC key component.
 
 **/
-BOOLEAN
+EFI_STATUS
 EFIAPI
-EcGetPubKey (
-  IN OUT  VOID   *EcContext,
+EcDhGetPubKey (
+  IN      VOID   *PKey,
+  IN      VOID   *EcGroup,
   OUT     UINT8  *PublicKey,
   IN OUT  UINTN  *PublicKeySize
   )
 {
-  EC_KEY         *EcKey;
-  CONST EC_GROUP *Group;
-  BOOLEAN        RetVal;
-  CONST EC_POINT *EcPoint;
-  BIGNUM         *BnX;
-  BIGNUM         *BnY;
-  INT32          OpenSslNid;
-  UINTN          HalfSize;
-  INTN           XSize;
-  INTN           YSize;
+  EC_KEY          *EcKey;
+  CONST EC_POINT  *EcPoint;
+  EFI_STATUS      Status;
+  BIGNUM          *BnX;
+  BIGNUM          *BnY;
+  INTN            XSize;
+  INTN            YSize;
+  UINTN           HalfSize;
 
-  if (EcContext == NULL || PublicKeySize == NULL) {
-    return FALSE;
+  if (PKey == NULL || EcGroup == NULL || PublicKeySize == NULL) {
+    return EFI_INVALID_PARAMETER;
   }
 
   if (PublicKey == NULL && *PublicKeySize != 0) {
-    return FALSE;
+    return EFI_INVALID_PARAMETER;
   }
 
-  EcKey = (EC_KEY *)EcContext;
-
-  OpenSslNid = EC_GROUP_get_curve_name(EC_KEY_get0_group(EcKey));
-  switch (OpenSslNid) {
-  case NID_X9_62_prime256v1:
-    HalfSize = 32;
-    break;
-  case NID_secp384r1:
-    HalfSize = 48;
-    break;
-  case NID_secp521r1:
-    HalfSize = 66;
-    break;
-  default:
-    return FALSE;
-  }
+  HalfSize = EcGroupGetPrimeBytes (EcGroup);
   if (*PublicKeySize < HalfSize * 2) {
     *PublicKeySize = HalfSize * 2;
-    return FALSE;
+    return EFI_INVALID_PARAMETER;
   }
   *PublicKeySize = HalfSize * 2;
 
-  Group = EC_KEY_get0_group (EcKey);
+  EcKey  = NULL;
+  BnX    = NULL;
+  BnY    = NULL;
+  Status = EFI_PROTOCOL_ERROR;
+
+  //EC_KEY* function will be deprecated in openssl 3.0, need update here when OpensslLib updating.
+  EcKey = EVP_PKEY_get1_EC_KEY (PKey);
+  if (EcKey == NULL) {
+    goto out;
+  }
+
   EcPoint = EC_KEY_get0_public_key (EcKey);
   if (EcPoint == NULL) {
-    return FALSE;
+    goto out;
   }
-   
+
   BnX = BN_new();
   BnY = BN_new();
   if (BnX == NULL || BnY == NULL) {
-    RetVal = FALSE;
-    goto Done;
+    goto out;
   }
-
-  RetVal = (BOOLEAN) EC_POINT_get_affine_coordinates(Group, EcPoint, BnX, BnY, NULL);
-  if (!RetVal) {
-    goto Done;
+  if (EC_POINT_get_affine_coordinates(EcGroup, EcPoint, BnX, BnY, NULL) != 1) {
+    goto out;
   }
-
   XSize = BN_num_bytes (BnX);
   YSize = BN_num_bytes (BnY);
   if (XSize <= 0 || YSize <= 0) {
-    RetVal = FALSE;
-    goto Done;
+    goto out;
   }
   ASSERT ((UINTN)XSize <= HalfSize && (UINTN)YSize <= HalfSize);
 
-  if (PublicKey != NULL) {
-    ZeroMem (PublicKey, *PublicKeySize);
-    BN_bn2bin (BnX, &PublicKey[0 + HalfSize - XSize]);
-    BN_bn2bin (BnY, &PublicKey[HalfSize + HalfSize - YSize]);
-  }
-  RetVal = TRUE;
+  ZeroMem (PublicKey, *PublicKeySize);
+  BN_bn2bin (BnX, &PublicKey[0 + HalfSize - XSize]);
+  BN_bn2bin (BnY, &PublicKey[HalfSize + HalfSize - YSize]);
 
-Done:
-  if (BnX != NULL) {
-    BN_free (BnX);
-  }
-  if (BnY != NULL) {
-    BN_free (BnY);
-  }
-  return RetVal;
+  Status = EFI_SUCCESS;
+out:
+  BN_free (BnX);
+  BN_free (BnY);
+  EC_KEY_free (EcKey);
+  return Status;
 }
 
 /**
@@ -351,103 +372,49 @@ EcCheckKey (
 BOOLEAN
 EFIAPI
 EcGenerateKey (
-  IN OUT  VOID   *EcContext,
-  OUT     UINT8  *Public,
-  IN OUT  UINTN  *PublicSize
+  IN       VOID  *EcGroup,
+  IN OUT   VOID  **PKey
   )
 {
-  EC_KEY         *EcKey;
-  CONST EC_GROUP *Group;
-  BOOLEAN        RetVal;
-  CONST EC_POINT *EcPoint;
-  BIGNUM         *BnX;
-  BIGNUM         *BnY;
-  INT32          OpenSslNid;
-  UINTN          HalfSize;
-  INTN           XSize;
-  INTN           YSize;
+  EFI_STATUS    Status;
+  INT32         Nid;
+  EVP_PKEY_CTX  *PKeyContext;
 
-  if (EcContext == NULL || PublicSize == NULL) {
-    return FALSE;
+  Status   = EFI_PROTOCOL_ERROR;
+  if (PKey == NULL || EcGroup == NULL) {
+    return EFI_INVALID_PARAMETER;
   }
 
-  if (Public == NULL && *PublicSize != 0) {
-    return FALSE;
-  }
+  Nid         = EC_GROUP_get_curve_name (EcGroup);
+  PKeyContext = EVP_PKEY_CTX_new_id(EVP_PKEY_EC, NULL);
 
-  EcKey = (EC_KEY *)EcContext;
-  RetVal = (BOOLEAN) EC_KEY_generate_key (EcKey);
-  if (!RetVal) {
-    return FALSE;
+  if (PKeyContext == NULL)
+      goto fail;
+  if (EVP_PKEY_keygen_init (PKeyContext) != 1) {
+    goto fail;
   }
-  OpenSslNid = EC_GROUP_get_curve_name(EC_KEY_get0_group(EcKey));
-  switch (OpenSslNid) {
-  case NID_X9_62_prime256v1:
-    HalfSize = 32;
-    break;
-  case NID_secp384r1:
-    HalfSize = 48;
-    break;
-  case NID_secp521r1:
-    HalfSize = 66;
-    break;
-  default:
-    return FALSE;
+  if (EVP_PKEY_CTX_set_ec_paramgen_curve_nid (PKeyContext, Nid) != 1) {
+    goto fail;
   }
-  if (*PublicSize < HalfSize * 2) {
-    *PublicSize = HalfSize * 2;
-    return FALSE;
+  // Assume RAND_seed was called
+  if (EVP_PKEY_keygen (PKeyContext, (EVP_PKEY **)PKey) != 1) {
+    goto fail;
   }
-  *PublicSize = HalfSize * 2;
+  Status = EFI_SUCCESS;
 
-  Group = EC_KEY_get0_group (EcKey);
-  EcPoint = EC_KEY_get0_public_key (EcKey);
-  if (EcPoint == NULL) {
-    return FALSE;
-  }
-
-  BnX = BN_new();
-  BnY = BN_new();
-  if (BnX == NULL || BnY == NULL) {
-    RetVal = FALSE;
-    goto Done;
-  }
-
-  RetVal = (BOOLEAN) EC_POINT_get_affine_coordinates(Group, EcPoint, BnX, BnY, NULL);
-  if (!RetVal) {
-    goto Done;
-  }
-
-  XSize = BN_num_bytes (BnX);
-  YSize = BN_num_bytes (BnY);
-  if (XSize <= 0 || YSize <= 0) {
-    RetVal = FALSE;
-    goto Done;
-  }
-  ASSERT ((UINTN)XSize <= HalfSize && (UINTN)YSize <= HalfSize);
-
-  if (Public != NULL) {
-    ZeroMem (Public, *PublicSize);
-    BN_bn2bin (BnX, &Public[0 + HalfSize - XSize]);
-    BN_bn2bin (BnY, &Public[HalfSize + HalfSize - YSize]);
-  }
-  RetVal = TRUE;
-
-Done:
-  if (BnX != NULL) {
-    BN_free (BnX);
-  }
-  if (BnY != NULL) {
-    BN_free (BnY);
-  }
-  return RetVal;
+fail:
+  EVP_PKEY_CTX_free (PKeyContext);
+  return Status;
 }
 
 /**
-  Computes exchanged common key.
+  Derive exchanged secret key.
 
-  Given peer's public key (X, Y), this function computes the exchanged common key,
+  Given peer's public key (X, Y), this function computes the exchanged secret key,
   based on its own context including value of curve parameter and random secret.
+  if used compressed coordinates, PeerPublic will only contain X, Y value is depend
+  on the value of IncY.
+  if not used compressed coordinates, IncY should be NULL, then
   X is the first half of PeerPublic with size being PeerPublicSize / 2,
   Y is the second half of PeerPublic with size being PeerPublicSize / 2.
 
@@ -473,100 +440,61 @@ Done:
   @retval FALSE  KeySize is not large enough.
 
 **/
-BOOLEAN
+EFI_STATUS
 EFIAPI
-EcComputeKey (
-  IN OUT  VOID         *EcContext,
-  IN      CONST UINT8  *PeerPublic,
-  IN      UINTN        PeerPublicSize,
-  OUT     UINT8        *Key,
-  IN OUT  UINTN        *KeySize
+EcDhDeriveSecret (
+  IN VOID    *PKey,
+  IN VOID    *EcGroup,
+  IN VOID    *PeerPKey,
+  OUT UINTN  *SecretSize,
+  OUT UINT8  *Secret
   )
 {
-  EC_KEY         *EcKey;
-  CONST EC_GROUP *Group;
-  BOOLEAN        RetVal;
-  BIGNUM         *BnX;
-  BIGNUM         *BnY;
-  EC_POINT       *Point;
-  INT32          OpenSslNid;
-  UINTN          HalfSize;
-  INTN           Size;
+  EVP_PKEY_CTX  *Ctx;
+  UINTN         Len;
+  EFI_STATUS    Status;
 
-  if (EcContext == NULL || PeerPublic == NULL || KeySize == NULL || Key == NULL) {
-    return FALSE;
+  if (PKey == NULL || EcGroup == NULL || PeerPKey == NULL || SecretSize == NULL) {
+    return EFI_INVALID_PARAMETER;
   }
 
-  if (PeerPublicSize > INT_MAX) {
-    return FALSE;
+  if ((Secret == NULL) && (*SecretSize != 0)) {
+    return EFI_INVALID_PARAMETER;
   }
 
-  EcKey = (EC_KEY *)EcContext;
-  OpenSslNid = EC_GROUP_get_curve_name(EC_KEY_get0_group(EcKey));
-  switch (OpenSslNid) {
-  case NID_X9_62_prime256v1:
-    HalfSize = 32;
-    break;
-  case NID_secp384r1:
-    HalfSize = 48;
-    break;
-  case NID_secp521r1:
-    HalfSize = 66;
-    break;
-  default:
-    return FALSE;
-  }
-  if (PeerPublicSize != HalfSize * 2) {
-    return FALSE;
+  if (!EVP_PKEY_public_check (PeerPKey)) {
+    return EFI_INVALID_PARAMETER;
   }
 
-  Group = EC_KEY_get0_group (EcKey);
-  Point = NULL;
+  Ctx     = NULL;
+  Status  = EFI_PROTOCOL_ERROR;
 
-  BnX = BN_bin2bn (PeerPublic, (UINT32) HalfSize, NULL);
-  BnY = BN_bin2bn (PeerPublic + HalfSize, (UINT32) HalfSize, NULL);
-  if (BnX == NULL || BnY == NULL) {
-    RetVal = FALSE;
-    goto Done;
-  }
-  Point = EC_POINT_new(Group);
-  if (Point == NULL) {
-    RetVal = FALSE;
-    goto Done;
+  Ctx = EVP_PKEY_CTX_new (PKey, NULL);
+  if (Ctx == NULL) {
+    goto fail;
   }
 
-  RetVal = (BOOLEAN) EC_POINT_set_affine_coordinates(Group, Point, BnX, BnY, NULL);
-  if (!RetVal) {
-    goto Done;
+  if ((EVP_PKEY_derive_init (Ctx) != 1) ||
+      (EVP_PKEY_derive_set_peer (Ctx, PeerPKey) != 1) ||
+      (EVP_PKEY_derive (Ctx, NULL, &Len) != 1))
+  {
+    goto fail;
   }
 
-  Size = ECDH_compute_key (Key, *KeySize, Point, EcKey, NULL);
-  if (Size < 0) {
-    RetVal = FALSE;
-    goto Done;
+  if (*SecretSize < Len) {
+    *SecretSize = Len;
+    Status = EFI_INVALID_PARAMETER;
+    goto fail;
+  }
+  *SecretSize = Len;
+
+  if (EVP_PKEY_derive (Ctx, Secret, &Len) != 1) {
+    goto fail;
   }
 
-  if (*KeySize < (UINTN) Size) {
-    *KeySize = Size;
-    RetVal = FALSE;
-    goto Done;
-  }
-
-  *KeySize = Size;
-
-  RetVal = TRUE;
-
-Done:
-  if (BnX != NULL) {
-    BN_free (BnX);
-  }
-  if (BnY != NULL) {
-    BN_free (BnY);
-  }
-  if (Point != NULL) {
-    EC_POINT_free(Point);
-  }
-  return RetVal;
+fail:
+  EVP_PKEY_CTX_free (Ctx);
+  return Status;
 }
 
 /**
