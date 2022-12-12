838d30b782300ca146b9e03a4adbda2322ad79c2
Qi Zhang <qi1.zhang@intel.com>
2022-12-12 12:57:21 +0800

    DeviceSecurityTestPkg： Add DeviceSecurity test support
    Signed-off-by: Jiewen Yao <jiewen.yao@intel.com>
    Signed-off-by: Wenxing Hou <wenxing.hou@intel.com>

---
+++ DeviceSecurityTestPkg/SpdmDeviceSecurityDxe/SpdmDeviceIo.c
@@ -0,0 +1,58 @@
+/** @file
+  EDKII Device Security library for SPDM device.
+  It follows the SPDM Specification.
+
+Copyright (c) 2020, Intel Corporation. All rights reserved.<BR>
+SPDX-License-Identifier: BSD-2-Clause-Patent
+
+**/
+
+#include "SpdmDeviceSecurityDxe.h"
+
+SPDM_RETURN
+SpdmDeviceSendMessage (
+  IN     VOID        *SpdmContext,
+  IN     UINTN       MessageSize,
+  IN     CONST VOID  *Message,
+  IN     UINT64      Timeout
+  )
+{
+  SPDM_DRIVER_DEVICE_CONTEXT  *SpdmDriverContext;
+  SPDM_IO_PROTOCOL            *SpdmIo;
+
+  SpdmDriverContext = GetSpdmDriverContextViaSpdmContext (SpdmContext);
+  if (SpdmDriverContext == NULL) {
+    ASSERT (SpdmDriverContext != NULL);
+    return LIBSPDM_STATUS_INVALID_PARAMETER;
+  }
+  SpdmIo = SpdmDriverContext->SpdmIoProtocol;
+  if (SpdmIo == NULL) {
+    return LIBSPDM_STATUS_SEND_FAIL;
+  }
+
+  return SpdmIo->SendMessage (SpdmIo, MessageSize, Message, Timeout);
+}
+
+SPDM_RETURN
+SpdmDeviceReceiveMessage (
+  IN     VOID    *SpdmContext,
+  IN OUT UINTN   *MessageSize,
+  IN OUT VOID    **Message,
+  IN     UINT64  Timeout
+  )
+{
+  SPDM_DRIVER_DEVICE_CONTEXT  *SpdmDriverContext;
+  SPDM_IO_PROTOCOL            *SpdmIo;
+
+  SpdmDriverContext = GetSpdmDriverContextViaSpdmContext (SpdmContext);
+  if (SpdmDriverContext == NULL) {
+    ASSERT (SpdmDriverContext != NULL);
+    return LIBSPDM_STATUS_INVALID_PARAMETER;
+  }
+  SpdmIo = SpdmDriverContext->SpdmIoProtocol;
+  if (SpdmIo == NULL) {
+    return LIBSPDM_STATUS_RECEIVE_FAIL;
+  }
+
+  return SpdmIo->ReceiveMessage (SpdmIo, MessageSize, Message, Timeout);
+}
