6e150ec8f779ee222d3f963a756d804ade0c4a87
Jean-Philippe Brucker <jean-philippe@linaro.org>
2023-05-04 12:37:35 +0100

    ArmSetMemoryAttributes: Don't clear address bits when setting page attributes
    An 'unprotected' bit set by ArmCcaConfigureMmio() may get cleared later
    by ArmSetMemoryAttributes(). For example the PCIe ECAM region on QEMU is
    mapped late by MapGcdMmioSpace(). Ensure the 'unprotected' bit does not
    get cleared.
    
    Note that we should probably consolidate this: ArmCcaConfigureMmio()
    initializes those page table entries as valid even though they don't
    have the correct memory attributes yet.
    
    Signed-off-by: Jean-Philippe Brucker <jean-philippe@linaro.org>

--- ArmPkg/Library/ArmMmuLib/AArch64/ArmMmuLibCore.c
+++ ArmPkg/Library/ArmMmuLib/AArch64/ArmMmuLibCore.c
@@ -539,7 +539,8 @@ ArmSetMemoryAttributes (
   UINT64  PageAttributeMask;
 
   PageAttributes    = GcdAttributeToPageAttribute (Attributes);
-  PageAttributeMask = 0;
+  /* Don't clear the address bits, which may contain NS attribute for a Realm */
+  PageAttributeMask = TT_ADDRESS_MASK_BLOCK_ENTRY;
 
   if ((Attributes & EFI_MEMORY_CACHETYPE_MASK) == 0) {
     //
@@ -547,8 +548,7 @@ ArmSetMemoryAttributes (
     // permissions only.
     //
     PageAttributes   &= TT_AP_MASK | TT_UXN_MASK | TT_PXN_MASK | TT_AF;
-    PageAttributeMask = ~(TT_ADDRESS_MASK_BLOCK_ENTRY | TT_AP_MASK |
-                          TT_PXN_MASK | TT_XN_MASK | TT_AF);
+    PageAttributeMask = ~(TT_AP_MASK | TT_PXN_MASK | TT_XN_MASK | TT_AF);
     if (AttributeMask != 0) {
       if (((AttributeMask & ~(UINT64)(EFI_MEMORY_RP|EFI_MEMORY_RO|EFI_MEMORY_XP)) != 0) ||
           ((Attributes & ~AttributeMask) != 0))
