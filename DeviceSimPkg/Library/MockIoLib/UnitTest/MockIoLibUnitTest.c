58a86d2e20b2acd9f72e7d5cb0aa6357cf0b2024
Mateusz Albecki <mateusz.albecki@intel.com>
2023-05-31 16:17:16 +0200

    Switch MockIoLibUnitTest to use new ByteEnable conversion function
    Signed-off-by: Mateusz Albecki <mateusz.albecki@intel.com>

--- DeviceSimPkg/Library/MockIoLib/UnitTest/MockIoLibUnitTest.c
+++ DeviceSimPkg/Library/MockIoLib/UnitTest/MockIoLibUnitTest.c
@@ -40,26 +40,7 @@ TestMockIoDeviceRead (
 {
   UINT32  ByteMask;
 
-  switch(ByteEnable) {
-    case 0x1:
-      ByteMask = 0xFF;
-      break;
-    case 0x2:
-      ByteMask = 0xFF00;
-      break;
-    case 0x3:
-      ByteMask = 0xFFFF;
-      break;
-    case 0x7:
-      ByteMask = 0xFFFFFF;
-      break;
-    case 0xC:
-      ByteMask = 0xFFFF0000;
-      break;
-    case 0xF:
-    default:
-      ByteMask = 0xFFFFFFFF;
-  }
+  ByteMask = ByteEnableToBitMask (ByteEnable);
 
   switch (Address) {
     case MOCK_IO_LIB_TEST_DEVICE_REG0_ADDRESS:
@@ -84,26 +65,7 @@ TestMockIoDeviceWrite (
 
   Device = (MOCK_IO_TEST_DEVICE_CONTEXT*) Context;
 
-  switch(ByteEnable) {
-    case 0x1:
-      ByteMask = 0xFF;
-      break;
-    case 0x2:
-      ByteMask = 0xFF00;
-      break;
-    case 0x3:
-      ByteMask = 0xFFFF;
-      break;
-    case 0x7:
-      ByteMask = 0xFFFFFF;
-      break;
-    case 0xC:
-      ByteMask = 0xFFFF0000;
-      break;
-    case 0xF:
-    default:
-      ByteMask = 0xFFFFFFFF;
-  }
+  ByteMask = ByteEnableToBitMask (ByteEnable);
 
   switch (Address) {
     case MOCK_IO_LIB_TEST_DEVICE_REG1_ADDRESS:
