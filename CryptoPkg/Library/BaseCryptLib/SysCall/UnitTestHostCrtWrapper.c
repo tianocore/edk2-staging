31676cc21e0cc1f5a56087426cf6d9b91c9f0089
sunceping <cepingx.sun@intel.com>
2023-06-15 14:00:37 +0800

    CryptoPkg: Enable the BIO_snprintf and BIO_printf
    VTPM TD Cert needs to generate the X509 cert, and X509 cert
    contain the valid time, it needs CryptoPkg enable the BIO_printf
    and BIO_snprintf.
    
    Signed-off-by: Sun Ceping <cepingx.sun@intel.com>

--- CryptoPkg/Library/BaseCryptLib/SysCall/UnitTestHostCrtWrapper.c
+++ CryptoPkg/Library/BaseCryptLib/SysCall/UnitTestHostCrtWrapper.c
@@ -76,26 +76,26 @@ sscanf (
 //  -- Dummy OpenSSL Support Routines --
 //
 
-int
-BIO_printf (
-  void        *bio,
-  const char  *format,
-  ...
-  )
-{
-  return 0;
-}
+// int
+// BIO_printf (
+//   void        *bio,
+//   const char  *format,
+//   ...
+//   )
+// {
+//   return 0;
+// }
 
-int
-BIO_snprintf (
-  char        *buf,
-  size_t      n,
-  const char  *format,
-  ...
-  )
-{
-  return 0;
-}
+// int
+// BIO_snprintf (
+//   char        *buf,
+//   size_t      n,
+//   const char  *format,
+//   ...
+//   )
+// {
+//   return 0;
+// }
 
 uid_t
 getuid (
