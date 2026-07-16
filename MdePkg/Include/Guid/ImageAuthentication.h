85b104b7f3006c14beb9e1289f751cd9c4a5cc1f
Jiewen Yao <jiewen.yao@intel.com>
2026-07-16 15:01:59 +0800

    MdePkg: Remove EFI_IMAGE_EXECUTION table definitions
    With the table's producer (DxeImageVerificationLib) and consumer
    (UefiShellDebug1CommandsLib) removed, delete the now-unused type and
    macro definitions for the EFI Image Execution Information Table from
    ImageAuthentication.h: EFI_IMAGE_EXECUTION_ACTION, the
    EFI_IMAGE_EXECUTION_AUTH_*/_POLICY_FAILED/_INITIALIZED/_AUTHENTICATION
    action codes, and the EFI_IMAGE_EXECUTION_INFO and
    EFI_IMAGE_EXECUTION_INFO_TABLE structures.
    
    Signed-off-by: Jiewen Yao <jiewen.yao@intel.com>

--- MdePkg/Include/Guid/ImageAuthentication.h
+++ MdePkg/Include/Guid/ImageAuthentication.h
@@ -428,68 +428,6 @@ typedef struct {
     0x4aafd29d, 0x68df, 0x49ee, {0x8a, 0xa9, 0x34, 0x7d, 0x37, 0x56, 0x65, 0xa7} \
   }
 
-// ***********************************************************************
-// Image Execution Information Table Definition
-// ***********************************************************************
-typedef UINT32 EFI_IMAGE_EXECUTION_ACTION;
-
-#define EFI_IMAGE_EXECUTION_AUTHENTICATION      0x00000007
-#define EFI_IMAGE_EXECUTION_AUTH_UNTESTED       0x00000000
-#define EFI_IMAGE_EXECUTION_AUTH_SIG_FAILED     0x00000001
-#define EFI_IMAGE_EXECUTION_AUTH_SIG_PASSED     0x00000002
-#define EFI_IMAGE_EXECUTION_AUTH_SIG_NOT_FOUND  0x00000003
-#define EFI_IMAGE_EXECUTION_AUTH_SIG_FOUND      0x00000004
-#define EFI_IMAGE_EXECUTION_POLICY_FAILED       0x00000005
-#define EFI_IMAGE_EXECUTION_INITIALIZED         0x00000008
-
-//
-// EFI_IMAGE_EXECUTION_INFO is added to EFI System Configuration Table
-// and assigned the GUID EFI_IMAGE_SECURITY_DATABASE_GUID.
-//
-typedef struct {
-  ///
-  /// Describes the action taken by the firmware regarding this image.
-  ///
-  EFI_IMAGE_EXECUTION_ACTION    Action;
-  ///
-  /// Size of all of the entire structure.
-  ///
-  UINT32                        InfoSize;
-  ///
-  /// If this image was a UEFI device driver (for option ROM, for example) this is the
-  /// null-terminated, user-friendly name for the device. If the image was for an application,
-  /// then this is the name of the application. If this cannot be determined, then a simple
-  /// NULL character should be put in this position.
-  /// CHAR16                    Name[];
-  ///
-
-  ///
-  /// For device drivers, this is the device path of the device for which this device driver
-  /// was intended. In some cases, the driver itself may be stored as part of the system
-  /// firmware, but this field should record the device's path, not the firmware path. For
-  /// applications, this is the device path of the application. If this cannot be determined,
-  /// a simple end-of-path device node should be put in this position.
-  /// EFI_DEVICE_PATH_PROTOCOL  DevicePath;
-  ///
-
-  ///
-  /// Zero or more image signatures. If the image contained no signatures,
-  /// then this field is empty.
-  /// EFI_SIGNATURE_LIST            Signature;
-  ///
-} EFI_IMAGE_EXECUTION_INFO;
-
-typedef struct {
-  ///
-  /// Number of EFI_IMAGE_EXECUTION_INFO structures.
-  ///
-  UINTN    NumberOfImages;
-  ///
-  /// Number of image instances of EFI_IMAGE_EXECUTION_INFO structures.
-  ///
-  // EFI_IMAGE_EXECUTION_INFO  InformationInfo[]
-} EFI_IMAGE_EXECUTION_INFO_TABLE;
-
 extern EFI_GUID  gEfiImageSecurityDatabaseGuid;
 extern EFI_GUID  gEfiCertSha256Guid;
 extern EFI_GUID  gEfiCertRsa2048Guid;
